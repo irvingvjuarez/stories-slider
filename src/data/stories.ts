8234e197c5ad8f496c0a88c65521e4ff8b26d431
Irving Juárez <irvingjuarez274@gmail.com>
2022-07-27 22:47:07 -0500

    All stories already added

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -2,6 +2,15 @@ import story1 from "@app/assets/stories/story1.jpg"
 import story2 from "@app/assets/stories/story2.jpg"
 import story3 from "@app/assets/stories/story3.jpg"
 import story4 from "@app/assets/stories/story4.jpg"
+import elder1 from "@app/assets/stories/elder1.jpg"
+import elder2 from "@app/assets/stories/elder2.jpg"
+import elder3 from "@app/assets/stories/elder3.jpg"
+import vacation1 from "@app/assets/stories/vacation1.jpg"
+import vacation2 from "@app/assets/stories/vacation2.jpg"
+import vacation3 from "@app/assets/stories/vacation3.jpg"
+import vacation4 from "@app/assets/stories/vacation4.jpg"
+import vacation5 from "@app/assets/stories/vacation5.jpg"
+
 
 export const STORIES = [
   {
@@ -12,6 +21,26 @@ export const STORIES = [
       story2,
       story3,
       story4
+    ]
+  },
+  {
+    id: 1,
+    author: 1,
+    stories: [
+      elder1,
+      elder2,
+      elder3
+    ]
+  },
+  {
+    id: 2,
+    author: 2,
+    stories: [
+      vacation1,
+      vacation2,
+      vacation3,
+      vacation4,
+      vacation5
     ]
   }
 ]
