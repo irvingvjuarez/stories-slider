dca24db63fd42674432434165239a7c7ec685567
Irving Juárez <irvingjuarez274@gmail.com>
2022-07-30 22:09:20 -0500

    Progress in the Timer lib, but a bug remaining

--- src/components/StoryImg/index.tsx
+++ src/components/StoryImg/index.tsx
@@ -6,7 +6,6 @@ import { StoriesContext } from "@app/contexts/StoriesContext"
 import { IAppContext, IStoriesContext } from "@app/contexts/types.interface"
 import { STORIES } from "@app/data/stories"
 import { USERS } from "@app/data/users"
-import { STORY_TIMING } from "@app/globals"
 import { REDUCER_TYPES, STORIES_REDUCER_TYPES } from "@app/reducers/types.enums"
 import { IPayload } from "@app/reducers/types.interface"
 import { toggleModal } from "@app/services/toggleModal"
@@ -20,52 +19,51 @@ interface StoryImgProps {
 const StoryImg: React.FC<StoryImgProps> = ({ imgUrl, storiesBatch }): JSX.Element => {
   const { storiesDispatch } = useContext(StoriesContext) as IStoriesContext
   const { dispatch, modal:{ userId } } = useContext(AppContext) as IAppContext
-  const cb = useCallback(() => console.log("Hi"), [])
 
-  useEffect(() => {
+  const { currentStoryIndex } = getCurrentStory(storiesBatch, imgUrl)
+  const cb = useCallback(() => {
+    Timer.pause()
+    const storiesRemaining = currentStoryIndex < storiesBatch.length - 1
+
+    if(storiesRemaining){
+      const newIndex = currentStoryIndex + 1
+
+      storiesDispatch?.({
+        type: STORIES_REDUCER_TYPES.setSingleStory,
+        content: storiesBatch[newIndex]
+      })
+    }
+
+    if(!storiesRemaining) {
+      const moreUsersStories = userId < STORIES.length - 1
+
+      if(moreUsersStories){
+        const newStoriesBatch = STORIES[userId + 1].stories
+        const userName = USERS.find(user => user.id === userId + 1)?.name
+
+        storiesDispatch?.({
+          type: STORIES_REDUCER_TYPES.setNewStoriesBatch,
+          config: { newStoriesBatch }
+        })
+
+        dispatch?.({ type: REDUCER_TYPES.setModalUser, config: {
+          userId: userId + 1,
+          userName
+        }})
+      }
+
+      if(!moreUsersStories) toggleModal(dispatch as React.Dispatch<IPayload>)
+    }
+  }, [])
+  const timerInitializer = useCallback(() => {
     new Timer(cb)
   }, [])
 
+  useEffect(() => timerInitializer(), [])
+  
   const handleLoad = () => {
-    const { currentStoryIndex } = getCurrentStory(storiesBatch, imgUrl)
     initTransition(storiesBatch[currentStoryIndex])
-    Timer.resume()
-
-    // Handling stories sliding
-    // setTimeout(() => {
-    //   const storiesRemaining = currentStoryIndex < storiesBatch.length - 1
-
-    //   if(storiesRemaining){
-    //     const newIndex = currentStoryIndex + 1
-
-    //     storiesDispatch?.({
-    //       type: STORIES_REDUCER_TYPES.setSingleStory,
-    //       content: storiesBatch[newIndex]
-    //     })
-    //   }
-
-    //   if(!storiesRemaining) {
-    //     const moreUsersStories = userId < STORIES.length - 1
-
-    //     if(moreUsersStories){
-    //       const newStoriesBatch = STORIES[userId + 1].stories
-    //       const userName = USERS.find(user => user.id === userId + 1)?.name
-
-    //       storiesDispatch?.({
-    //         type: STORIES_REDUCER_TYPES.setNewStoriesBatch,
-    //         config: { newStoriesBatch }
-    //       })
-
-    //       dispatch?.({ type: REDUCER_TYPES.setModalUser, config: {
-    //         userId: userId + 1,
-    //         userName
-    //       }})
-    //     }
-
-    //     if(!moreUsersStories) toggleModal(dispatch as React.Dispatch<IPayload>)
-    //   }
-
-    // }, STORY_TIMING)
+    Timer.resume(true)
   }
 
   return(
--- src/libs/Timer/index.ts
+++ src/libs/Timer/index.ts
@@ -1,6 +1,8 @@
+import { STORY_TIMING } from "@app/globals"
+
 export class Timer {
   static id: number | null
-  static timing: number = 5000
+  static timing: number = STORY_TIMING
   static start: number
   static callback: () => void
 
@@ -8,11 +10,14 @@ export class Timer {
     Timer.callback = callback
   }
 
-  static resume() {
+  static resume(firstLoad?: boolean) {
     if(!Timer.id){
       console.log("Resume")
       Timer.id = window.setTimeout(this.callback, Timer.timing)
-      Timer.start = Date.now()
+      if(firstLoad){
+        // Timer.timing = STORY_TIMING
+        Timer.start = Date.now()
+      }
 
       console.log({ id: Timer.id })
     }
@@ -22,13 +27,16 @@ export class Timer {
     if(Timer.id){
       console.log("Pause")
       window.clearTimeout(Timer.id)
-      Timer.timing -= Date.now() - Timer.start
-      setTimeout(() => Timer.id = null, 1000)
+      let timeRemaining = Timer.timing < STORY_TIMING ? Timer.timing - (STORY_TIMING - Timer.timing) : Timer.timing - (Date.now() - Timer.start)
+      console.log({ timeRemaining })
 
-      console.log({
-        timing: Timer.timing,
-        id: Timer.id
-      })
+      Timer.timing = Timer.timing <= 0 ? STORY_TIMING : timeRemaining
+      setTimeout(() => Timer.id = null, 0)
+
+      // console.log({
+      //   timing: Timer.timing,
+      //   id: Timer.id,
+      // })
     }
   }
 }
