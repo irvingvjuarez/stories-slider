452917f1df47ac882105973af90727e0eb4884ab
Irving Juárez <irvingjuarez274@gmail.com>
2022-08-04 14:16:22 -0500

    The StoryButton has a callback useful for mobile and wider layouts

--- src/components/StoryButton/index.tsx
+++ src/components/StoryButton/index.tsx
@@ -9,11 +9,23 @@ const StoryButton: React.FC<StoryButtonProps> = ({
   children,
   direction = "right"
 }): JSX.Element => {
+  const handleChangeStory = (triggerEl: "container" | "wrapper") => () => {
+    if(window.screen.width >= 640 && triggerEl === "container") return
+
+    console.log("Changing story...")
+  }
+
   return(
-    <div className={getInitialClassName(direction)} >
-      <div className="bg-black rounded-full hidden sm:block">
+    <div
+      className={getInitialClassName(direction)}
+      onClick={handleChangeStory("container")}
+    >
+      <button
+        className="bg-black rounded-full hidden sm:block"
+        onClick={handleChangeStory("wrapper")}
+      >
         {children}
-      </div>
+      </button>
     </div>
   )
 }
