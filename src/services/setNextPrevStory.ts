d916757440ebc093731e82f8aef1a25929911459
Irving Juárez <irvingjuarez274@gmail.com>
2022-08-07 17:03:30 -0500

    Identifying if there is room to move to prev/next stories when clicked buttons

--- src/services/setNextPrevStory.ts
+++ src/services/setNextPrevStory.ts
@@ -1,5 +1,28 @@
+import { getCurrentStory } from "@app/containers/StoriesHover/utils"
+import { STORIES } from "@app/data/stories"
 import { INextPrevStory } from "@app/types/interfaces/nextPrevStoryConfig.interface"
 
 export const setNextPrevStory = (config: INextPrevStory) => {
-  console.log(config)
+  const { currentStories, currentStory, direction, userId } = config
+  const { currentStoryIndex } = getCurrentStory(currentStories, currentStory)
+
+  if(direction === "left"){
+    const storiesInLeft = currentStoryIndex > 0
+    const authorsInLeft = userId > 0
+    const goingLeft = storiesInLeft || authorsInLeft
+
+    console.log({
+      goingLeft
+    })
+
+  } else {
+    const storiesInRight = currentStoryIndex < currentStories.length - 1
+    const authorsInRight = userId < STORIES.length - 1
+    const goingRight = storiesInRight || authorsInRight
+    
+    console.log({
+      goingRight
+    })
+
+  }
 }
