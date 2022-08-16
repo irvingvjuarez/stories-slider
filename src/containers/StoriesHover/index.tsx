eeae48feaa868cdd934929e2227f60353736a1c1
Irving Juárez <irvingjuarez274@gmail.com>
2022-08-15 22:09:45 -0500

    Everything works fine except the pause and loading

--- src/containers/StoriesHover/index.tsx
+++ src/containers/StoriesHover/index.tsx
@@ -3,13 +3,26 @@ import React, { useContext, useEffect } from "react"
 import { StoryImg } from "@app/components/StoryImg"
 import { StoriesContext } from "@app/contexts/StoriesContext"
 import { IStoriesContext } from "@app/types/interfaces/storiesContext.interface"
+import { useStoryPause } from "@app/hooks/useStoryPause"
+import { StoryBubble } from "@app/components/StoryBubble"
+import { PostIcon } from "@app/components/PostIcon"
+import { MdOutlineClear } from "react-icons/md"
+import { FaPause, FaPlay } from "react-icons/fa"
+import { IPayload } from "@app/types/interfaces/payload.interface"
 
-interface StoriesHoverProps {
-  children: JSX.Element
-}
+import { handlePause, handleClick } from "./utils"
 
-const StoriesHover: React.FC<StoriesHoverProps> = ({ children }): JSX.Element => {
+const StoriesHover: React.FC = (): JSX.Element => {
   const { currentStories, currentStory, loading } = useContext(StoriesContext) as IStoriesContext
+
+  const {
+    storyTransitionConfig,
+    inPause,
+    dispatch,
+    userName,
+    userAvatar,
+    userId
+  } = useStoryPause()
 
   useEffect(() => {
     const spanElement = document.getElementById(currentStory)
@@ -31,7 +44,30 @@ const StoriesHover: React.FC<StoriesHoverProps> = ({ children }): JSX.Element =>
             ))}
           </div>
 
-          {children}
+          <div className="p-3 flex justify-between items-center w-full">
+            <StoryBubble
+              imgUrl={userAvatar}
+              userName={userName as string}
+              userId={userId}
+              isPost={true}
+              width="w-10"
+              height="h-10"
+            />
+
+            <div className="flex items-center space-x-2">
+              <button onClick={handlePause(storyTransitionConfig)}>
+                <PostIcon
+                  iconFn={() => inPause ? FaPlay : FaPause}
+                  iconSize="text-sm"
+                  iconSizeMd="text-xl"
+                />
+              </button>
+
+              <button onClick={handleClick(dispatch as React.Dispatch<IPayload>)}>
+                <PostIcon iconFn={() => MdOutlineClear} />
+              </button>
+            </div>
+          </div>
         </div>
       </StoryImg>
     </section>
--- src/portals/StoryPortal/index.tsx
+++ src/portals/StoryPortal/index.tsx
@@ -1,62 +1,18 @@
 import React from "react"
-import { MdOutlineClear } from "react-icons/md"
-import { FaPause, FaPlay } from "react-icons/fa"
-import { PostIcon } from "@app/components/PostIcon"
-import { StoryBubble } from "@app/components/StoryBubble"
 import { StoriesHover } from "@app/containers/StoriesHover"
 
 import { StoriesContext } from "@app/contexts/StoriesContext"
 import { IStoriesContext } from "@app/types/interfaces/storiesContext.interface"
 
-import { handlePause, handleClick } from "./utils"
-import { IPayload } from "@app/types/interfaces/payload.interface"
 import { useStoryPause } from "@app/hooks/useStoryPause"
-import { FULL_STORIES_DATA } from "@app/data/fullStoriesData"
 
 const StoryPortal: React.FC = (): JSX.Element => {
-  const {
-    storiesStateInitialValue,
-    storyTransitionConfig,
-    inPause,
-    dispatch,
-    userName,
-    userAvatar,
-    userId
-  } = useStoryPause()
-
-  console.log({
-    FULL_STORIES_DATA
-  })
+  const { storiesStateInitialValue } = useStoryPause()
 
   return(
     <StoriesContext.Provider value={storiesStateInitialValue as IStoriesContext}>
       <section className="fixed top-0 w-full h-screen bg-black">
-        <StoriesHover>
-          <div className="p-3 flex justify-between items-center w-full">
-            <StoryBubble
-              imgUrl={userAvatar}
-              userName={userName as string}
-              userId={userId}
-              isPost={true}
-              width="w-10"
-              height="h-10"
-            />
-
-            <div className="flex items-center space-x-2">
-              <button onClick={handlePause(storyTransitionConfig)}>
-                <PostIcon
-                  iconFn={() => inPause ? FaPlay : FaPause}
-                  iconSize="text-sm"
-                  iconSizeMd="text-xl"
-                />
-              </button>
-
-              <button onClick={handleClick(dispatch as React.Dispatch<IPayload>)}>
-                <PostIcon iconFn={() => MdOutlineClear} />
-              </button>
-            </div>
-          </div>
-        </StoriesHover>
+        <StoriesHover />
       </section>
     </StoriesContext.Provider>
   )
