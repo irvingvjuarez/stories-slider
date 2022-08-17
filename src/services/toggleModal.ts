8979ff46aaa60596a2521879dcf48ba0b7df6f92
Irving Juárez <irvingjuarez274@gmail.com>
2022-08-16 22:49:49 -0500

    Fixed some errors with interfaces paths

--- src/services/toggleModal.ts
+++ src/services/toggleModal.ts
@@ -1,6 +1,7 @@
 import { toggleOverflowHidden } from "./toggleOverflowHidden"
 import { REDUCER_TYPES } from "@app/reducers/types.enums"
-import { IConfig, IPayload } from "@app/reducers/types.interface"
+import { IPayload } from "@app/types/interfaces/payload.interface"
+import { IConfig } from "@app/types/interfaces/config.interface"
 
 export const toggleModal = (dispatch: React.Dispatch<IPayload>, config?: IConfig) => {
   toggleOverflowHidden("body")
