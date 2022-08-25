bf27ee00eca55b97991f662402a8e68f02b703e6
Irving Juárez <irvingjuarez274@gmail.com>
2022-08-24 23:41:02 -0500

    Changes in the design without affecting the behavior of the app

--- src/containers/StoriesHover/index.tsx
+++ src/containers/StoriesHover/index.tsx
@@ -20,18 +20,20 @@ const StoriesHover: React.FC<StoriesHoverProps> = ({ children }): JSX.Element =>
   
   return(
     <section className="max-w-[900px] mx-auto">
-      <div className="flex justify-between pt-3 space-x-1 px-1">
-        {currentStories.map(story => (
-          <div key={story} className="w-full relative">
-            <span className="story-hover"></span>
-            <span className="absolute top-0 left-0 story-hover" id={story}></span>
+      <StoryImg imgUrl={currentStory}>
+        <div className="absolute px-2 w-[97%] bg-gradient-to-t from-transparent to-black">
+          <div className="flex justify-between pt-3 space-x-1 px-1">
+            {currentStories.map(story => (
+              <div key={story} className="w-full relative">
+                <span className="story-hover"></span>
+                <span className="absolute top-0 left-0 story-hover" id={story}></span>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      {children}
-
-      <StoryImg imgUrl={currentStory} />
+          {children}
+        </div>
+      </StoryImg>
     </section>
   )
 }
